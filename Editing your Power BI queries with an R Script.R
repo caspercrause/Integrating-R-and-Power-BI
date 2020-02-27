ed860eadb270cd69ae6abf7454c7c629a9d13002
caspercrause <ccrause07@gmail.com>
2020-02-27 08:00:07 +0200

    fixing spelling errors

--- Editing your Power BI queries with an R Script.R
+++ Editing your Power BI queries with an R Script.R
@@ -3,6 +3,6 @@ library(data.table)
 setDT(dataset)
 
 
-# Merge only the column names that have contain digits "\\d" into one column called 'year' and merge the values to a column called 'sensus.result'
+# Merge only the column names that contain digits "\\d" into one column called 'year' and merge the values to a column called 'sensus.result'
 melt(dataset,measure.vars = patterns("\\d"),variable.name = 'year',value.name = 'sensus.result'
 )[, sensus.result := ifelse(test = is.na(sensus.result),yes = 0,no = sensus.result)] -> output
